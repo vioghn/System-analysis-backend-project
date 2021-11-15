c6906c8d46c95346d4199d6e1dde19dbfd04efb6
aynr99 <aynourja@gmail.com>
2021-11-15 15:18:25 +0330

    account/views(check user id to fill the fav field)

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from goodreads.models import Book
+# Createour views here.
 
-# Create your views here.
+
+
+@ login_required
+def favourite_add(request, id):
+    book = get_object_or_404(Book, id=id)
+    if book.favourites.filter(id=request.user.id).exists():
+        book.favourites.remove(request.user)
+    else:
+        book.favourites.add(request.user)
+    return HttpResponseRedirect(request.Meta['HTTP_REFERER'])

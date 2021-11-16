69eaab723b8282bb32f5d23a544458248bb2868e
zahra <sualehi98@gmail.com>
2021-11-16 20:40:33 +0330

    eror
    eror

--- book/urls.py
+++ book/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BookSearch, FilterCategory
+from .views import BookSearch, FilterCategory, show_books
 from . import views
 
 urlpatterns = [
     path("addbook/", views.add_book, name="addbook"),
-    path("loadbook/", views.show_books, name="show_books"),
-    path('booksearch/', views.BookSearch.as_view(), name="BookSearch"),
+    path("loadbook/", views.show_books.as_view(), name="show_books"),
+    path('search/', views.BookSearch.as_view(), name="BookSearch"),
     path('category/', FilterCategory.as_view(), name="FilterCategory"),
 ]
 
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from django.http import Http404
 from .models import AddBook
 from .serializers import BookSerializer
@@ -9,26 +10,54 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 
 @api_view(['POST'])
+@permission_classes([])
+@authentication_classes([])
 def add_book(request):
     data = dict(request.POST)
     print(data)
+    addbook = AddBook.objects.filter(title=data['title'][0])
+    if list(addbook) != []:
+        return Response({'message': 'book has added'})
     addbook = AddBook.objects.create(      
         title=data['title'][0],
         genre=data['genre'][0],
         authors=data['authors'][0],
-        publisher=data['publisher'][0],
         publication_date=data['publication_date'][0],
     )
     addbook.save()
     if "Description" in request.data.keys() :
         addbook.Description=data['Description'][0]
     addbook.save()
+    if "publisher" in request.data.keys() :
+        addbook.publisher=data['publisher'][0]
+    addbook.save()
+    if "Base64" in request.data.keys() :
+        base64=data['Base64'][0] 
+        newsrc = open('book/image/' + str(addbook.id) + '.txt', 'a')
+        newsrc.write(base64)
+        newsrc.close()
+        
+    addbook.bookAvatar = 'book/image/' + str(addbook.id) + '.txt'
+    addbook.save()
     return Response({'message': 'New book added'}, status=status.HTTP_201_CREATED)
     
 
 
+class show_books(generics.ListAPIView):
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    def get_queryset(self):
+        queryset = AddBook.objects.all()
+        return queryset
+
+
+
 @api_view(['POST'])
-def show_books(request):
+@permission_classes([])
+@authentication_classes([])
+def show_booksdddd(request):
     books = AddBook.objects.all()
     data = []
     print(books)
@@ -40,13 +69,13 @@ def show_books(request):
 
 class BookSearch(generics.ListAPIView):
     serializer_class = BookSerializer
-
+    permission_classes = [AllowAny]
+    authentication_classes = []
 
 
     def get_queryset(self):
         searchedword = self.request.query_params.get('q', None)
         queryset = AddBook.objects.all()
-        print("searchedword",searchedword)
         if searchedword is None:
             return queryset
         if searchedword is not None:
@@ -67,9 +96,11 @@ class BookSearch(generics.ListAPIView):
 
 class FilterCategory(generics.ListAPIView):
     serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = []
 
     def get_queryset(self):
         q = self.request.query_params.get('q', None)
-        queryset = AddBook.objects.filter(Q(genre=q) | Q(publication_date=q))
+        queryset = AddBook.objects.filter(Q(genre=q) | Q(authors=q))
         return queryset
-
+        
